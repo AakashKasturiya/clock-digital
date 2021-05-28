2ad6f791e41b60517714c714d041ee8a5681e315
aakash kasturiya <aakashkasturiya@gmail.com>
2021-05-28 20:15:55 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Video from './Video/video.mp4';
 import './App.css';
 
 function App() {
+
+
+let cTime = new Date().toLocaleTimeString();
+
+let [time,setTime] = useState(cTime);
+
+
+function RefTime()
+{
+   let NewTime = new Date().toLocaleTimeString();
+   setTime(NewTime)
+}
+
+setInterval(RefTime);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <video id="myVideo" autoPlay loop>
+      <source src = {Video} type="video/mp4" />
+    </video>
+      <header className="content">
+
+        <h1>{time}</h1>
+    
       </header>
     </div>
   );
